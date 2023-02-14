5ba8ca6b02f497257d236889dba8a93e723f5f87
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-02-14 11:36:56 +0500

    Added in the Project Interface items

---
+++ Interface/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Interface
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool isMarried { get; set; }
+    }
+}
