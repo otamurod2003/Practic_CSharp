0fa428eb74d538d46210329bedfb2f3db94de127
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-02-16 15:30:09 +0500

    changed the location directories

---
+++ NumberOfDivisors/Program.cs
@@ -0,0 +1,23 @@
+﻿//Kiritilgan sonning bo'luvchilarini topuvchi dastur
+long number;
+Console.WriteLine("Son kiriting: ");
+number = Convert.ToInt64(Console.ReadLine());
+long[] divisors = new long[number];
+divisors[0] = 1;
+Console.WriteLine("Sonning 1 dan tashqari bo'luvchilari");
+try
+{
+    for (int i = 1; i < divisors.Length; i++)
+    {
+        divisors[i] = i + 1;
+        if (number % divisors[i] == 0)
+            Console.Write(divisors[i] + " ");
+    }
+
+}
+catch (Exception ex)
+{
+
+    Console.Write("Error:" + ex.Message);
+}
+
---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿//Kiritgan sonni tub yoki tub emasligini aniqlab beruvchi dastur
+//int number;
+//Console.WriteLine("Butun son kiriting: ");
+//number = Convert.ToInt32(Console.ReadLine());
+
+//for(int i = 2; i < number; i++)
+//{
+//    if((number % 1 == 0)&&(number % number == 0))
+//        Console.WriteLine("Tub son");
+//    else
+//        Console.WriteLine("Tub son emas");break;
+//}
+//int son = 1;
+//do
+//{
+//    if (number % son == 0)
+//        Console.WriteLine("Tub emas");
+//    else
+//        Console.WriteLine("Tub");
+//    son++;
+//}
+//while (son >= number);
+
+bool isPrime(int number)
+{
+    if (number == 1) return false;
+    if (number == 2) return true;
+
+    var limit = Math.Ceiling(Math.Sqrt(number)); //hoisting the loop limit
+
+    for (int i = 2; i <= limit; ++i)
+        if (number % i == 0)
+            return false;
+    return true;
+
+}
+
+Console.WriteLine(isPrime(17));
+
+Console.WriteLine(Math.Pow(3.872983346207417,2));
