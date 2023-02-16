5d8982d0efbcf0110a0f2d66767dc43ada5657a3
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-02-16 11:52:37 +0500

    Added directory "Exercise". Added to solution ConsoleApp project

---
+++ Exercises/Max.Min.Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿int max, min, numberOfDigits;
+int[] numbers;
+Console.Write("Nechta son kiritmoqchisiz: ");
+numberOfDigits = Convert.ToInt32(Console.ReadLine());
+numbers = new int[numberOfDigits];
+try
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{i + 1} sonni kiriting: ");
+
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    max = numbers[0];
+    min = numbers[0];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] < min)
+            min = numbers[i];
+
+        if (numbers[i] > max)
+            max = numbers[i];
+
+    }
+
+    Console.WriteLine($"Max= {max}, Min={min}");
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine("Xatolik: " + ex.Message);
+}
