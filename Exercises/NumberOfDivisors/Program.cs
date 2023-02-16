e11e49370a40c0ca3c146294944a34fe042f62b5
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-02-16 12:37:13 +0500

    Added NumberOfDivisors console application

---
+++ Exercises/NumberOfDivisors/Program.cs
@@ -0,0 +1,25 @@
+﻿int number,divisors,i;
+int[] numbers;
+
+try
+{
+    //Console.Write("Son kiriting:");
+    //number = Convert.ToInt32(Console.ReadLine());
+    //numbers = new int[number];
+    //for (i = 1; i < numbers.Length; i++)
+    //{
+    //    numbers[i] = number / i;
+    //}
+
+    //Console.WriteLine(numbers[i]);
+
+    number = 15 % 5;
+    if (number == 0)
+        Console.WriteLine("Zo'r");
+    else Console.WriteLine("Rasvo");
+}
+catch (Exception ex)
+{
+
+	Console.Write("Xatolik: " + ex.Message);
+}
