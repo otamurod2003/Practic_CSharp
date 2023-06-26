ac69d26020f7f560a94ab9abe6b7e2d8daa81316
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-06-26 18:49:43 +0500

    Lambda expressions with LINQ methods added

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LambdaExpressions
+{
+    public class Progam
+    {
+        public static void Main(string[] args)
+        {
+            int[] sonlar = { 15, 25, 16, 12, 18, 20, 19, 21, 22, 23, 24, };
+
+            var sonlarKvadrati = sonlar.Select(x => x * x);
+            Console.WriteLine(string.Join(",", sonlarKvadrati));
+
+
+            List<string> towers = new List<string> { "Tokio", "Kuala Lumpur", "Buenos-Ayres", "Tashkent", "Jakarta", "Moskva" };
+
+            var sortedTowersByLength = towers.Where(text=> text.Length>8); // sorted by length
+            sortedTowersByLength = towers.Where(text => text.StartsWith('B'));
+
+            Console.WriteLine(string.Join(",", sortedTowersByLength));
+
+            var orderedTowers = towers.OrderBy(o=> o);
+            Console.WriteLine(string.Join("  ", orderedTowers));
+        }
+    }
+}
