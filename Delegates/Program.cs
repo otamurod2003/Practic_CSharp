6e8fc110c2fc76526779fd771f2d69ccdb740485
Otamurod Pirnapasov <otamurodpirnapasov2003@gmail.com>
2023-06-26 17:46:48 +0500

    Added delegates features in csharp

---
+++ Delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Delegates
+{
+    public class Program
+    {
+        public delegate void Method(string s);
+        public static void MyMethod(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+        public static void Main(string[] args)
+        {
+            Method salom = new Method(MyMethod);
+            salom("asdadwedwewefwef");
+            salom.Invoke("salom qalleee");
+        }
+
+    }
+}
